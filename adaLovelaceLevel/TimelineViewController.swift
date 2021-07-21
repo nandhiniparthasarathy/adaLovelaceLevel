4247f4bd51480eb8be81e1a2d98a69a99830a2fd
Hannah Rappaport <hannahgrappaport@gmail.com>
2021-07-20 20:27:39 -0400

    added timeline!

---
+++ adaLovelaceLevel/TimelineViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TimelineViewController.swift
+//  adaLovelaceLevel
+//
+//  Created by  Scholar on 7/20/21.
+//
+
+import UIKit
+
+class TimelineViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
