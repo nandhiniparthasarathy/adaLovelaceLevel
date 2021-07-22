552f125c7b24e35adbfa3f015bf8de347eb6ebd7
Elise Ikejiani <eliseikejiani@gmail.com>
2021-07-21 19:16:35 -0500

    Woo! Camera finally works! lol

--- adaLovelaceLevel/ViewController.swift
+++ adaLovelaceLevel/ViewController.swift
@@ -7,13 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate{
 
+    
+    @IBOutlet weak var displayImage: UIImageView!
+    
+    var imagePicker = UIImagePickerController()
     override func viewDidLoad() {
+        imagePicker.delegate = self
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
+    
+    
+    @IBAction func TAP (_ sender: Any) {
+        imagePicker.sourceType = .camera; present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func PLT (_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary; present(imagePicker, animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let selectedImage =  info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+        displayImage.image = selectedImage
+        imagePicker.dismiss(animated: true, completion: nil)
+    
+  
+//
+   
+    
+}
 
 }
 
+}
+   
+
+
+//
+//
